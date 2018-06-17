5c0c37b58a462b1136494d41916a0e2ecaf230ed
lrokey <rokey94@gmail.com>
2018-06-17 14:37:54 -0500

    add view object to get rid of annoying alert boxes

--- js/main.js
+++ js/main.js
@@ -4,6 +4,20 @@ const quiz = [
 	{english: "cat", spanish: "gato"}
 ];
 
+// view object
+const view = {
+	score: document.querySelector("#score strong"),
+	question: document.getElementById("question"),
+	result: document.getElementById("result"),
+	info: document.getElementById("info"),
+	render(target, content, attributes) {
+		for (const key in attributes) {
+			target.setAttribute(key, attributes[key]);
+		}
+		target.innerHTML = content;
+	}
+};
+
 const game = {
 	start(quiz) {
 		this.questions = [...quiz];
@@ -21,6 +35,7 @@ const game = {
 
 	ask() {
 		const question = `What is ${this.question.english} in Spanish?`;
+		view.render(view.question, question);
 		const response = prompt(question);
 		this.check(response);
 	},
@@ -28,15 +43,18 @@ const game = {
 	check(response) {
 		const answer = this.question.spanish
 		if (response === answer) {
+			view.render(view.result, "Correct!", {"class": "correct"});
 			alert("Correct!");
 			this.score++;
+			view.render(view.score, this.score);
 		} else {
+			view.render(view.result, `Wrong! The correct answer was ${answer}`, {"class": "wrong"});
 			alert(`Wrong! The correct answer was ${answer}`);
 		}
 	},
 
 	gameOver() {
-		alert(`Quiz Over, you scored ${this.score} point${this.score !== 1 ? 's' : ''}`);
+		view.render(view.info, `Quiz Over, you scored ${this.score} point${this.score !== 1 ? 's' : ''}`);
 	}
 }
 game.start(quiz);
