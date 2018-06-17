0bab8406fb7f3252920ce110add97c37fced9e4c
lrokey <rokey94@gmail.com>
2018-06-17 14:43:29 -0500

    add start button

--- js/main.js
+++ js/main.js
@@ -10,16 +10,24 @@ const view = {
 	question: document.getElementById("question"),
 	result: document.getElementById("result"),
 	info: document.getElementById("info"),
+	start: document.getElementById("start"),
 	render(target, content, attributes) {
 		for (const key in attributes) {
 			target.setAttribute(key, attributes[key]);
 		}
 		target.innerHTML = content;
+	},
+	show(element) {
+		element.style.display = "block";
+	},
+	hide(element) {
+		element.style.display = "none";
 	}
 };
 
 const game = {
 	start(quiz) {
+		view.hide(view.start);
 		this.questions = [...quiz];
 		this.score = 0;
 
@@ -52,9 +60,10 @@ const game = {
 			alert(`Wrong! The correct answer was ${answer}`);
 		}
 	},
-
 	gameOver() {
+		view.show(view.start);
 		view.render(view.info, `Quiz Over, you scored ${this.score} point${this.score !== 1 ? 's' : ''}`);
 	}
 }
-game.start(quiz);
+
+view.start.addEventListener("click", () => game.start(quiz), false);
